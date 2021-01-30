dc1d2f0b7e100dc9606d328574b7140c8f6a542c
TheDodo24 <dodogaming.info@gmail.com>
2021-01-30 18:47:25 +0100

    Salary and maxEmployees can be now saved in Team

--- common/src/main/java/de/thedodo24/commonPackage/player/Teams.java
+++ common/src/main/java/de/thedodo24/commonPackage/player/Teams.java
@@ -7,15 +7,15 @@ import org.bukkit.ChatColor;
 @Getter
 public enum Teams {
 
-    HELFER("helfer", "helfer-duty","ingi-leiter", "Eventhelfer", "team.mod.bau", "team-helfer", ChatColor.GOLD),
-    MVA("mva", "mva-duty", "mva-leiter", "MVA", "team.mod.medien", "team-mva", ChatColor.DARK_PURPLE),
-    FBT("fbt", "fbt-duty", "fbt-leiter","FBT", "team.mod.bau", "team-fbt", ChatColor.YELLOW),
-    POLIZEI("polizist", "polizist-duty", "poli-leiter", "Polizei", "team.mod.justiz", "team-polizist", ChatColor.AQUA),
-    SUPPORTER("supporter", "supporter-duty","sup-leiter","Supporter", "team.mod.community", "team-supporter", ChatColor.GREEN),
-    INGENIEUR("ingenieur", "ingenieur-duty", "ingi-leiter", "Ingenieur", "team.mod.bau", "team-ingenieur", ChatColor.BLUE),
-    DEVELOPER("developer", "developer-duty", "dev-leiter", "Developer", "team.mod.technik", "team-developer", ChatColor.RED),
-    MODERATOR("moderator", "moderator-duty", "administrator", "Moderator", "team.admin", "team-mod", ChatColor.DARK_GREEN),
-    ADMINISTRATOR("administrator", "administrator-duty","administrator","Administrator", "team.admin", "team-sl", ChatColor.DARK_RED);
+    HELFER("helfer", "helfer-duty","ingi-leiter", "Eventhelfer", "team.mod.bau", "team-helfer", ChatColor.GOLD, 15000 * 100, 3),
+    MVA("mva", "mva-duty", "mva-leiter", "MVA", "team.mod.medien", "team-mva", ChatColor.DARK_AQUA, 15000 * 100, 5),
+    FBT("fbt", "fbt-duty", "fbt-leiter","FBT", "team.mod.bau", "team-fbt", ChatColor.YELLOW, 7500 * 100, 4),
+    POLIZEI("polizist", "polizist-duty", "poli-leiter", "Polizei", "team.mod.justiz", "team-polizist", ChatColor.AQUA, 20000 * 100, 5),
+    SUPPORTER("supporter", "supporter-duty","sup-leiter","Supporter", "team.mod.community", "team-supporter", ChatColor.GREEN, 20000 * 100, 6),
+    INGENIEUR("ingenieur", "ingenieur-duty", "ingi-leiter", "Ingenieur", "team.mod.bau", "team-ingenieur", ChatColor.BLUE, 40000 * 100, 4),
+    DEVELOPER("developer", "developer-duty", "dev-leiter", "Developer", "team.mod.technik", "team-developer", ChatColor.RED, 45000 * 100, 4),
+    MODERATOR("moderator", "moderator-duty", "administrator", "Moderator", "team.admin", "team-mod", ChatColor.DARK_GREEN, 0, 0),
+    ADMINISTRATOR("administrator", "administrator-duty","administrator","Administrator", "team.admin", "team-sl", ChatColor.DARK_RED, 0, 0);
 
     String permName;
     String dutyName;
@@ -24,8 +24,10 @@ public enum Teams {
     String permission;
     String bankAccount;
     ChatColor color;
+    long salary;
+    int maxEmployees;
 
-    Teams(String permName, String dutyName, String leiterGroup, String displayName, String permission, String bankAccount, ChatColor color) {
+    Teams(String permName, String dutyName, String leiterGroup, String displayName, String permission, String bankAccount, ChatColor color, int salary, int maxEmployees) {
         this.permName = permName;
         this.dutyName = dutyName;
         this.leiterGroup = leiterGroup;
@@ -33,6 +35,8 @@ public enum Teams {
         this.permission = permission;
         this.bankAccount = bankAccount;
         this.color = color;
+        this.salary = salary;
+        this.maxEmployees = maxEmployees;
     }
 
 }
