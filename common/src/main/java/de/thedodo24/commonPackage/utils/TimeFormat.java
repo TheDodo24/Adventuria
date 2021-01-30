90b8eac4613a1aeb4503a1bdafdb39e72215c468
TheDodo24 <dodogaming.info@gmail.com>
2021-01-30 18:48:39 +0100

    New timeformats

--- common/src/main/java/de/thedodo24/commonPackage/utils/TimeFormat.java
+++ common/src/main/java/de/thedodo24/commonPackage/utils/TimeFormat.java
@@ -1,5 +1,10 @@
 package de.thedodo24.commonPackage.utils;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
+
 public class TimeFormat {
 
     public static String getString(long time) {
@@ -36,8 +41,18 @@ public class TimeFormat {
         return (minutes == 1 ? minutes + " Minute" : minutes + " Minuten");
     }
 
+    public static double getInMinutesString(long time) {
+        long minutes = ((long) (((double) time / (1000 * 60)) * 100));
+        return ((Long) minutes).doubleValue() / 100;
+    }
+
     public static int getInHours(long time) {
-        return ((int) (((double) time / (1000 * 60 * 60)) * 100));
+        return ((int) (((double) time / (1000 * 60 * 60))));
+    }
+
+    public static double getInHoursDouble(long time) {
+        long h =  ((int) (((double) time / (1000 * 60 * 60)) * 100));
+        return ((Long) h).doubleValue() / 100;
     }
 
     public static int getInMinutes(long time) {
@@ -53,6 +68,47 @@ public class TimeFormat {
         else
             hour = " Stunden";
         return ((Long) hours).doubleValue() / 100 + hour;
+    }
+
+    public static final int ticksAtMidnight = 18000;
+    public static final int ticksPerDay = 24000;
+    public static final int ticksPerHour = 1000;
+    public static final double ticksPerMinute = 1000d / 60d;
+    public static final double ticksPerSecond = 1000d / 60d / 60d;
+
+    public static Date ticksToDate(long ticks)
+    {
+        // Assume the server time starts at 0. It would start on a day.
+        // But we will simulate that the server started with 0 at midnight.
+        ticks = ticks - ticksAtMidnight + ticksPerDay;
+
+        // How many ingame days have passed since the server start?
+        final long days = ticks / ticksPerDay;
+        ticks -= days * ticksPerDay;
+
+        // How many hours on the last day?
+        final long hours = ticks / ticksPerHour;
+        ticks -= hours * ticksPerHour;
+
+        // How many minutes on the last day?
+        final long minutes = (long)Math.floor(ticks / ticksPerMinute);
+        final double dticks = ticks - minutes * ticksPerMinute;
+
+        // How many seconds on the last day?
+        final long seconds = (long)Math.floor(dticks / ticksPerSecond);
+
+        // Now we create an english GMT calendar (We wan't no daylight savings)
+        final Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("GMT"), Locale.GERMAN);
+        cal.setLenient(true);
+
+        // And we set the time to 0! And append the time that passed!
+        cal.set(0, Calendar.JANUARY, 1, 0, 0, 0);
+        cal.add(Calendar.DAY_OF_YEAR, (int)days);
+        cal.add(Calendar.HOUR_OF_DAY, (int)hours);
+        cal.add(Calendar.MINUTE, (int)minutes);
+        cal.add(Calendar.SECOND, (int)seconds + 1); // To solve rounding errors.
+
+        return cal.getTime();
     }
 
     /*public static String getString(long time) {
