1cc49db6ddfb8c26a1d66b5717888d7d62043f05
TheDodo24 <dodogaming.info@gmail.com>
2021-01-30 18:42:44 +0100

    Module enable service updated

--- bootstrap/src/main/java/de/thedodo24/Adventuria/Adventuria.java
+++ bootstrap/src/main/java/de/thedodo24/Adventuria/Adventuria.java
@@ -12,6 +12,7 @@ import de.thedodo24.commonPackage.module.Module;
 import de.thedodo24.commonPackage.towny.Town;
 import lombok.Getter;
 import de.thedodo24.commonPackage.module.ModuleManager;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 import de.thedodo24.commonPackage.utils.ConfigFile;
 
@@ -30,32 +31,33 @@ public class Adventuria extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        instance = this;
+            instance = this;
 
-        ConfigFile config = new ConfigFile(getDataFolder(), "config.yml");
-        config.create("database.host", "localhost");
-        config.create("database.user", "user");
-        config.create("database.password", "password");
-        config.create("database.database", "database");
-        config.create("database.port", 8529);
+            ConfigFile config = new ConfigFile(getDataFolder(), "config.yml");
+            config.create("database.host", "localhost");
+            config.create("database.user", "user");
+            config.create("database.password", "password");
+            config.create("database.database", "database");
+            config.create("database.port", 8529);
 
-        config.create("mysql.host", "localhost");
-        config.create("mysql.user", "user");
-        config.create("mysql.password", "password");
-        config.create("mysql.database", "database");
-        config.create("mysql.port", 3306);
-        config.save();
+            config.create("mysql.host", "localhost");
+            config.create("mysql.user", "user");
+            config.create("mysql.password", "password");
+            config.create("mysql.database", "database");
+            config.create("mysql.port", 3306);
+            config.save();
 
-        arangoDB = new ArangoDB.Builder().host(config.getString("database.host"), config.getInt("database.port"))
-                .user(config.getString("database.user")).password(config.getString("database.password")).build();
-        arangoDatabase = arangoDB.db(config.getString("database.database"));
+            arangoDB = new ArangoDB.Builder().host(config.getString("database.host"), config.getInt("database.port"))
+                    .user(config.getString("database.user")).password(config.getString("database.password")).build();
+            arangoDatabase = arangoDB.db(config.getString("database.database"));
 
-        mySQL = new MySQL(config.getString("mysql.host"), config.getString("mysql.user"), config.getString("mysql.password"), config.getString("mysql.database"),
-                config.getInt("mysql.port"));
-        mySQL.createInstance();
+            mySQL = new MySQL(config.getString("mysql.host"), config.getString("mysql.user"), config.getString("mysql.password"), config.getString("mysql.database"),
+                    config.getInt("mysql.port"));
+            mySQL.createInstance();
 
-        moduleManager = new ModuleManager("modules", getArangoDatabase(),this, mySQL);
-        moduleManager.loadModules(Common.class, Economy.class, Jail.class, Utils.class, Towny.class);
+            moduleManager = new ModuleManager("modules", getArangoDatabase(),this, mySQL);
+            moduleManager.loadModules(Common.class, Economy.class, Utils.class, Towny.class);
+        Bukkit.getScheduler().scheduleAsyncDelayedTask(this, () -> moduleManager.loadModule(Jail.class), 2L);
     }
 
     @Override
